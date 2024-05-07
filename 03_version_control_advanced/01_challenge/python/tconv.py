6b0d40b0b8e433dc452efef75d233401d557de7a
Jiro Fujino <jiro.fujino@woven-planet.global>
2024-05-07 17:09:55 +0900

    added func main()

--- 03_version_control_advanced/01_challenge/python/tconv.py
+++ 03_version_control_advanced/01_challenge/python/tconv.py
@@ -11,3 +11,22 @@ def convert_fahrenheit_to_celsius():
     fahrenheit = int(fahrenheit)
     celsius = 5/9*(fahrenheit - 32) 
     print(f"The Fahrenheit temperature {fahrenheit} you entered is {celsius} in Celsius.")
+    
+def main():
+    print("Enter c if you want to convert from Fahrenheit to Celsius")
+    print("Enter f if you want to convert from Celsius to Fahrenheit")
+    is_valid = False
+    while not is_valid:
+        c_or_f = input()
+        if c_or_f == "c":
+            is_valid = True
+            convert_fahrenheit_to_celsius()
+        elif c_or_f == "f":
+            is_valid = True
+            convert_celsius_to_fahrenheit()
+        else:
+            print("Incorrect input. Please try again later")
+            continue
+
+
+main()
